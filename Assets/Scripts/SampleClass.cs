59e3873be3f4f275ca5b8d855602caa6f40932d8
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-26 23:42:44 +0800

    initial commit

---
+++ Assets/Scripts/SampleClass.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+/// <summary>
+/// This is a sample class intended for testing
+/// </summary>
+public class SampleClass : MonoBehaviour
+{
+
+    /// <summary>
+    /// Executes a method.
+    /// </summary>
+    private void SampleMethod()
+    {
+        // Do code magic here
+    }
+
+    /// <summary>
+    /// Executes a method and returns an int
+    /// </summary>
+    /// <returns>The int returned by this method</returns>
+    private int SampleReturnMethod()
+    {
+        return 0;
+    }
+
+}
